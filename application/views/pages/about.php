f1974d4dff2387e9e6e8bbf90de60405beb851ca
hadv <dvietha@gmail.com>
2017-10-14 17:37:09 +0700

    Adding route for some static pages

---
+++ application/views/pages/about.php
@@ -0,0 +1,3 @@
+In that directory, create two files named home.php and about.php. Within those files, type some text − anything you’d like − and save them. If you like to be particularly un-original, try “Hello World!”.
+
+
---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+		<br>
+		<hr>
+		<em>ClevverMail &copy; 2017</em>
+	</body>
+</html>
